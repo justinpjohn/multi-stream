80a307bfdec13d3c0d1bbcb9c7e34ea95a3c6168
Andrew Wong <ndrew.wong@gmail.com>
2022-06-19 20:04:28 -0500

    Update packages

---
+++ client/src/app/store.ts
@@ -0,0 +1,24 @@
+import { configureStore, ThunkAction, AnyAction } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import counterReducer from "../features/counter/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  AnyAction
+>;
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
